fb20cbfdf0073616e61f99b6ea60b34ecb4dcc1e
Kazumasa Yamamoto <yamamoto_kazumasa@uniquevision.co.jp>
2019-10-02 14:14:54 +0900

    バグ修正

--- web.rb
+++ web.rb
@@ -452,10 +452,10 @@ module Isucari
 
       items = if item_id > 0 && created_at > 0
         # paging
-        db.xquery("SELECT t1.* FROM `items` AS t1 LEFT OUTER JOIN `users` AS t2 ON t1.`seller_id` = t2.`id` WHERE t1.`seller_id` = ? AND t1.`status` IN (?, ?, ?) AND t1.`created_at` <= ? AND t1.`id` < ? ORDER BY t1.`created_at` DESC, t1.`id` DESC LIMIT #{ITEMS_PER_PAGE + 1}", user_simple['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, Time.at(created_at), item_id)
+        db.xquery("SELECT t1.*, t2.`account_name`, t2.`num_sell_items` FROM `items` AS t1 LEFT OUTER JOIN `users` AS t2 ON t1.`seller_id` = t2.`id` WHERE t1.`seller_id` = ? AND t1.`status` IN (?, ?, ?) AND t1.`created_at` <= ? AND t1.`id` < ? ORDER BY t1.`created_at` DESC, t1.`id` DESC LIMIT #{ITEMS_PER_PAGE + 1}", user_simple['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT, Time.at(created_at), item_id)
       else
         # 1st page
-        db.xquery("SELECT t1.* FROM `items` AS t1 LEFT OUTER JOIN `users` AS t2 ON t1.`seller_id` = t2.`id` WHERE t1.`seller_id` = ? AND t1.`status` IN (?, ?, ?) ORDER BY t1.`created_at` DESC, t1.`id` DESC LIMIT #{ITEMS_PER_PAGE + 1}", user_simple['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT)
+        db.xquery("SELECT t1.*, t2.`account_name`, t2.`num_sell_items` FROM `items` AS t1 LEFT OUTER JOIN `users` AS t2 ON t1.`seller_id` = t2.`id` WHERE t1.`seller_id` = ? AND t1.`status` IN (?, ?, ?) ORDER BY t1.`created_at` DESC, t1.`id` DESC LIMIT #{ITEMS_PER_PAGE + 1}", user_simple['id'], ITEM_STATUS_ON_SALE, ITEM_STATUS_TRADING, ITEM_STATUS_SOLD_OUT)
       end
 
       item_simples = items.map do |item|
